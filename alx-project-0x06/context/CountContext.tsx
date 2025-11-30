b703d6add877e28a3bfdb2c852abe698dab4ba4f
Peter Wangimwa <peterwangimwa@gmail.com>
2025-11-30 19:23:21 +0300

    fix: update CountContext interface and README

--- alx-project-0x06/context/CountContext.tsx
+++ alx-project-0x06/context/CountContext.tsx
@@ -1,9 +1,9 @@
-import { createContext, ReactNode, useContext, useState } from "react"
+import { createContext, ReactNode, useContext, useState } from "react";
 
 interface CountContextProps {
-    count: number
-    increment: () => void
-    decrement: () => void
+    count: number;
+    increment: () => void;
+    decrement: () => void;
 }
 
 export const CountContext = createContext<CountContextProps | undefined>(undefined)
